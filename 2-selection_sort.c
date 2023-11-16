f4762737b7b68d0cc69b25bcdf53cc0a88b344fb
Anis Emad <anisemad123@gmail.com>
2023-11-16 12:53:15 +0200

    fixed betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,25 +6,25 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, minj;
-    int temp;
+	size_t i, j, minj;
+	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        minj = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[minj])
-            {
-                minj = j;
-            }
-        }
-        if (minj != i)
-        {
-            temp = array[i];
-            array[i] = array[minj];
-            array[minj] = temp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		minj = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minj])
+			{
+				minj = j;
+			}
+		}
+		if (minj != i)
+		{
+			temp = array[i];
+			array[i] = array[minj];
+			array[minj] = temp;
+			print_array(array, size);
+		}
+	}
 }
