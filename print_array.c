e7479b193bb95d83df094f83165c258e85ae1a96
malakehab2003 <lokaehab2003@gmail.com>
2023-11-17 17:57:00 +0200

    try fix error

--- print_array.c
+++ print_array.c
@@ -1,13 +1,13 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "sort.h"
+
 /**
  * print_array - Prints an array of integers
  *
  * Return: void
  * @array: The array to be printed
  * @size: Number of elements in @array
- */
+*/
+
 void print_array(const int *array, size_t size)
 {
 	size_t i;
